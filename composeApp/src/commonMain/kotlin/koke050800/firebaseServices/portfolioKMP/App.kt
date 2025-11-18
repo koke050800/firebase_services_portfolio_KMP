a304c0e94eb59edcbe5d39b9e81e8527994763b3
Jorge Saucedo <koke050800@gmail.com>
2025-11-18 15:07:39 -0600

    fix: Package name

--- composeApp/src/commonMain/kotlin/koke050800/firebaseServices/portfolioKMP/App.kt
+++ composeApp/src/commonMain/kotlin/koke050800/firebaseServices/portfolioKMP/App.kt
@@ -13,7 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.firebaseServices.portfolioKMP.theme.AppTheme
+import koke050800.firebaseServices.portfolioKMP.theme.AppTheme
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
