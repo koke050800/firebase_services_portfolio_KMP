0270e83d73d6e5bd73869f18ab6527c373f19ee3
Jorge Saucedo <koke050800@gmail.com>
2025-11-20 02:11:50 -0600

    Fix(AppDelegate iOS): Typo in AppDelegate application method
    Corrected parameter name from '_application' to '_ application' in AppDelegate.swift and removed unnecessary print statement for cleaner initialization.

--- iosApp/iosApp/AppDelegate.swift
+++ iosApp/iosApp/AppDelegate.swift
@@ -4,10 +4,8 @@ import ComposeApp
 
 class AppDelegate: NSObject, UIApplicationDelegate {
 
-    func application(_application: UIApplication,
+    func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-
-        print("AppDelegate: didFinishLaunchingWithOptions - Calling KMP init.")
 
         FirebaseHelper.shared.doInit()
 
